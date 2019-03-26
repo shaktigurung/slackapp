2c120761b7837cd60db095e93ae76d0b83855607
shaktigurung <mr.shakti.gurung@gmail.com>
2019-03-26 14:46:24 +1100

    roomlist added

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react'
 export default class RoomList extends Component {
   render() {
     const rooms = this.props.rooms;
+    console.log(rooms);
     return (
       <div>
          <h1> Room List </h1>
+         <p> {this.props.currentRoom.name} (current room )</p>
          <ul>
         {rooms.map(room => {
             return (<li key={room.id}>  <a href="#"> {room.name} </a> </li>)
